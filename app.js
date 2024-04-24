0f46599c1f77af5372c55fce2c510cf665333ec3
Jeetender1621 <160727550+Jeetender1621@users.noreply.github.com>
2024-04-24 20:26:47 +0530

    Add changes for rest of the sections

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const navLinks = document.querySelector('.nav-links');
 
 hamBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    if (window.innerWidth <= 700) {
+    if (window.innerWidth <= 900) {
         hamBtn.style.display = 'none'; // Hide hamburger icon
         closeBtn.style.display = 'block'; // Show close icon
     }
@@ -12,7 +12,7 @@ hamBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    if (window.innerWidth <= 700) {
+    if (window.innerWidth <= 900) {
         hamBtn.style.display = 'block'; // Show hamburger icon
         closeBtn.style.display = 'none'; // Hide close icon
     }
@@ -20,7 +20,7 @@ closeBtn.addEventListener('click', () => {
 
 // Add an event listener for window resize to adjust the display of icons
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 700) {
+    if (window.innerWidth > 900) {
         // Hide both icons on larger screens
         hamBtn.style.display = 'none';
         closeBtn.style.display = 'none';
